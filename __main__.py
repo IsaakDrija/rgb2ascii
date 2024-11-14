caddc5237fedc78f7f7cc4c9083c59f706f7d4cf
IsaakDrija <drijamedinai@gmail.com>
2024-11-14 11:21:13 +0000

    Created very basic layout.

--- __main__.py
+++ __main__.py
@@ -3,4 +3,4 @@
 if __name__ == "__main__":
     print("Hola mundo!") #Just testing haha
     import ui
-    # The UI comes last.
+    # The UI comes last on load, hence why it's here.
--- ui/__init__.py
+++ ui/__init__.py
@@ -3,29 +3,50 @@ import gi
 # Defines the necessary GTK version on the system
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+# Please refer to https://python-gtk-3-tutorial.readthedocs.io/en/latest/
+# for the PyGTK documentation
+
 
 class main_window(Gtk.Window):
     def __init__(self):
         """Initializes the main window. Usually only called one per execution."""
         super().__init__(title = "RGB 2 ASCII")
 
-        self.main_box = Gtk.Box(spacing=6)
+        # The bigger box, parent of the other widgets
+        self.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         self.add(self.main_box)
 
-        self.menu_bar = Gtk.MenuBar()
-        self.main_box.pack_start(self.menu_bar, True, True, 0)
+        # Another box, so as to arrange thing either vertically (append to main)
+        # or horizontally (append to this one)
+        self.secondary_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
 
-        self.button1 = Gtk.Button(label="Botón 1")
-        self.main_box.pack_start(self.button1, True, True, 0)
-        #self.menu = Gtk.MenuBar()
-        #self.add(self.menu)
-        #self.load_button = Gtk.FileChooserButton()
-        # self.load_button.connect('clicked', self.load_image_clicked)
-        #self.add(self.load_button)
+        # TODO: Make this useable!
+        self.load_button = Gtk.FileChooserButton()
+        self.load_button.connect('file-set', self.image_loaded)
 
-    def load_image_clicked(self, widget):
-        """Opens the OS file explorer"""
-        print("Apurao")
+        #The left side panel with all of its elements
+        self.left_panel = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+
+        # The central image to load
+        self.main_image = Gtk.Image()
+
+        # The right side panel with all of its contents
+        self.right_panel = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+
+
+        # Finally, adding the elements to the window
+        self.main_box.pack_start(self.load_button, True, True, 0)
+        self.main_box.pack_end(self.secondary_box, True, True, 0)
+        self.secondary_box.pack_start(self.left_panel, True, True, 0)
+        self.secondary_box.pack_start(self.main_image, True, True, 0)
+        self.secondary_box.pack_end(self.right_panel, True, True, 0)
+
+
+    def image_loaded(self, widget):
+        """Loads the image after the user has selected one"""
+        # TODO: Have this make something, probably customizing the chooser instance
+        print("Module: UI, Image loaded successfully")
+        self.main_image.set_from_file("./apurao.jpg")
 
 #The module loaded and, thus, you see the window
 print("Module: UI successfully started.\n")
